6bcdd44bd52f82d7f126f99b7aa94fc4b4a13739
somin <qkrthals0524@naver.com>
2021-05-12 22:04:49 +0900

    Add Reservations Admin

--- rooms/models.py
+++ rooms/models.py
@@ -89,10 +89,11 @@ class Room(core_models.TimeStampedModel):
 
     def total_rating(self):
         all_reviews = self.reviews.all()
-        try:
-            all_ratings = 0
-            for review in all_reviews:
-                all_ratings += review.rating_average()
-                return all_ratings / len(all_reviews)
-        except ZeroDivisionError:
+        all_ratings = 0
+        for review in all_reviews:
+            all_ratings += review.rating_average()
+
+        if len(all_reviews) > 0:
+            return all_ratings / len(all_reviews)
+        else:
             return 0
